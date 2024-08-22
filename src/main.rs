cddc56cce96ff873928d1ab062b304fc7220f3c6
Ólafur Waage <olafurw@gmail.com>
2024-08-22 20:26:32 +0200

    deunicodification

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,12 @@
-use std::collections::BTreeSet;
-use std::fs::File;
-use std::io::{prelude::*, BufReader};
-mod json_parsing;
+use deunicode::deunicode;
+use utils::{word_minus_ur, words_to_vec};
 
-fn words_to_vec(filename: &str) -> BTreeSet<String> {
-    let file = File::open(filename).unwrap();
-    let reader = BufReader::new(file);
-    let mut words = BTreeSet::new();
-
-    for line in reader.lines() {
-        words.insert(line.unwrap());
-    }
-
-    return words;
-}
-
-fn word_minus_ur(is_word: &str) -> String {
-    let ends_ur = is_word.ends_with("ur");
-    if !ends_ur {
-        return is_word.to_string();
-    }
-
-    let is_word = &is_word[..is_word.len() - 2];
-    is_word.to_string()
-}
+mod utils;
 
 fn main() {
-    let is_words = words_to_vec("is.words");
-    let other_words = words_to_vec("nb.words");
+    let text = "åfløyer";
+    let sktext = deunicode(text);
     
-    for word in is_words {
-        let word_no_ur = word_minus_ur(&word);
-        if other_words.contains(&word) || other_words.contains(&word_no_ur) {
-            println!("{}", word);
-        }
-    }
+    println!("Original: {}", text);
+    println!("NFC: {}", sktext);
 }
--- src/utils.rs
+++ src/utils.rs
@@ -2,13 +2,35 @@ use std::collections::BTreeSet;
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 
-fn extract_word(json_line: &String) -> String {
+pub fn words_to_vec(filename: &str) -> BTreeSet<String> {
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+    let mut words = BTreeSet::new();
+
+    for line in reader.lines() {
+        words.insert(line.unwrap());
+    }
+
+    return words;
+}
+
+pub fn word_minus_ur(is_word: &str) -> String {
+    let ends_ur = is_word.ends_with("ur");
+    if !ends_ur {
+        return is_word.to_string();
+    }
+
+    let is_word = &is_word[..is_word.len() - 2];
+    is_word.to_string()
+}
+
+pub fn extract_word(json_line: &String) -> String {
     let json_line = json::parse(json_line.as_str()).unwrap();
     let word = json_line["word"].as_str().unwrap();
     return word.to_string().to_lowercase();
 }
 
-fn word_is_alphabetic(word: &String) -> bool {
+pub fn word_is_alphabetic(word: &String) -> bool {
     for c in word.chars() {
         if !c.is_alphabetic() {
             return false;
